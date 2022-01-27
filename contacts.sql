0eb7e6a85508358597b7d0492b1612c3db385087
artkalugin <64751684+artkalugin@users.noreply.github.com>
2022-01-27 16:09:12 +0500

    update contacts.sql

--- contacts.sql
+++ contacts.sql
@@ -1,9 +1,9 @@
-CREATE TABLE contactsss
+CREATE TABLE contacts
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
-  phone varchar(20) NOT NULL,
+  phone varchar(12) NOT NULL,
   role varchar(30)
 );
 
-INSERT INTO contactsss SET name='Артем', phone='+79122081147', role='Разработчик справочника';
+INSERT INTO contacts SET name='Артем', phone='+79122081147', role='Разработчик справочника';
