ad0e3e86a4f53f54f141280fe607680afb3b1f92
artkalugin <artkalugin27@gmail.com>
2022-01-27 16:19:27 +0500

    create separate file for js

---
+++ script.js
@@ -0,0 +1,53 @@
+$('#addContactForm').submit(function() {
+    $.post(
+    'handler.php',
+    $('#addContactForm').serialize(),		
+    function(msg) {
+        $('#contact-list').append(msg);
+        $('#addClose').click();
+        $('#addContactForm').trigger('reset');
+    }
+    )
+    .fail(function(msg) {
+    alert('Ошибка записи. Пожалуйста, обратитесь к администратору.');
+    });
+    return false;
+});
+
+$('#editContactForm').submit(function() {
+    $.post(
+    'handler.php',
+    $('#editContactForm').serialize(),		
+    function(msg) {
+        id = $('#idField').val();
+        $('.contact-list__row[data-id="' + id + '"]').replaceWith(msg);
+        $('#editClose').click();
+        $('#editContactForm').trigger('reset');
+    }
+    )
+    .fail(function(msg) {
+    alert('Ошибка записи. Пожалуйста, обратитесь к администратору.');
+    });;
+    return false;
+});
+
+function fill_form(id) {
+    $('#editNameField').val($('.contact-list__row[data-id="' + id + '"] .contact-list__name').html());
+    $('#editPhoneField').val($('.contact-list__row[data-id="' + id + '"] .contact-list__phone').html());
+    $('#editRoleField').val($('.contact-list__row[data-id="' + id + '"] .contact-list__role').html());
+    $('#idField').val(id);
+}
+
+function remove_row(id) {
+    $.ajax({
+        method: "POST",
+        url: "delete.php",
+        data: { id: id }
+        })
+      .done(function(msg) {
+        $('.contact-list__row[data-id=' + id + ']').remove();
+      })
+    .fail(function(msg) {
+    alert('Ошибка удаления. Пожалуйста, обратитесь к администратору.');
+    });;
+};
